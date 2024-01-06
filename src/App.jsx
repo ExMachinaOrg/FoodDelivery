4ad23b783a8f7594205254146312ea988ead9e12
HughOwenPanopio <panopiohughowen@gmail.com>
2024-01-06 21:13:24 +0800

    #1-Navbar: Created Header, 2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import theme from "./shared/Theme";
+import Navbar from "./components/Navbar/Index";
 import Header from "./components/Header/Index";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <ThemeProvider theme={theme}>
       <header>
         <Header />
+        <Navbar />
       </header>
       <main>
         <Outlet />
